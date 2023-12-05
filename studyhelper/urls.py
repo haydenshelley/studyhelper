7c3b3c4f2f47772e51b27fe7ee0b02010d57360f
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-12-05 11:12:13 -0800

    configure room url

--- studyhelper/urls.py
+++ studyhelper/urls.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 def home(req):
   return HttpResponse('Home Page')
 
+def room(req):
+  return HttpResponse('Room')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('room/', room)
 ]
