120b17a6e3de826da96480a13285ecead824168c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-12-05 11:36:18 -0800

    room context dictionary

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms = [
+  {'id': 1, 'name': 'Lets Learn Python!'},
+  {'id': 2, 'name': 'Design with Me'},
+  {'id': 3, 'name': 'Frontend Developers'},
+]
+
 def home(req):
-  return render(req, 'home.html')
+  return render(req, 'home.html', {'rooms': rooms})
 
 def room(req):
   return render(req, 'room.html')
