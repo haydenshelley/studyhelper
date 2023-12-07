5b16ed0130380c497dce8820f422dbea6c0714f9
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-12-07 11:45:44 -0800

    create room form

--- base/views.py
+++ base/views.py
@@ -12,3 +12,7 @@ def room(req, pk):
   room = Room.objects.get(id=pk)
   context = {'room': room}
   return render(req, 'base/room.html', context)
+
+def createRoom(req):
+  context = {}
+  return render(req, 'base/room_form.html', context)
